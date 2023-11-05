2b7362f4c7ae6e8bf700f909b69ef13198c613d4
Masaya Nakatsuka <nmsuteado2@yahoo.co.jp>
2023-11-05 21:41:18 +0900

    settings.gradle.ktsへ必要な依存関係の記述

--- src/backend/build.gradle.kts
+++ src/backend/build.gradle.kts
@@ -20,6 +20,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
