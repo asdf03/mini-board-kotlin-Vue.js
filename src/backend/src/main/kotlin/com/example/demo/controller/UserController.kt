d5a242b8660275ace896f64c7cceb232010e68eb
Masaya Nakatsuka <nmsuteado2@yahoo.co.jp>
2023-11-12 11:45:35 +0900

    UserControllerを作成

---
+++ src/backend/src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -0,0 +1,41 @@
+package com.demo.controller
+
+import com.demo.model.User
+import com.demo.service.UserService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/users")
+class UserController(private val userService: UserService) {
+
+  @PostMapping
+  fun createUser(@RequestBody user: User): ResponseEntity<User> {
+    val createdUser = userService.createUser(user)
+    return ResponseEntity.ok(createdUser)
+  }
+
+  @GetMapping
+  fun getAllUsers(): ResponseEntity<List<User>> {
+    val users = userService.findAllUsers()
+    return ResponseEntity.ok(users)
+  }
+
+  @GetMapping("/{id}")
+  fun getUserById(@PathVariable id: Long): ResponseEntity<User> {
+    val user = userService.findUserById(id)
+    return ResponseEntity.ok(user)
+  }
+
+  @PutMapping("/{id}")
+  fun updateUser(@PathVariable id: Long, @RequestBody updatedUser: User): ResponseEntity<User>{
+    val updateUser = userService.findUserById(id, updatedUser)
+    return ResponseEntity.ok(user)
+  }
+
+  @DeleteMapping("/{id}")
+  fun deleteUser(@PathVariable id: Long): ResponseEntity<Void> {
+    userService.deleteUser(id)
+    return ResponseEntity.ok().build()
+  }
+}
