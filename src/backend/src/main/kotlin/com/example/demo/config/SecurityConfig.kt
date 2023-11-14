216501323e5ffb1811a3ef3d687359e31327c611
Masaya Nakatsuka <nmsuteado2@yahoo.co.jp>
2023-11-14 11:49:03 +0900

    backendのconfigを修正

--- src/backend/src/main/kotlin/com/example/demo/config/SecurityConfig.kt
+++ src/backend/src/main/kotlin/com/example/demo/config/SecurityConfig.kt
@@ -2,22 +2,20 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
   @Bean
-  fun passwordEncoder(): PasswordEncoder {
-    return BCryptPasswordEncoder()
-  }
-
-  @Bean
   fun filterChain(http: HttpSecurity): SecurityFilterChain {
     http
+      .cors().configurationSource(corsConfigurationSource())
+      .and()
       .csrf().disable()
       .authorizeHttpRequests { authz ->
         authz
@@ -29,5 +27,17 @@ class SecurityConfig {
       .formLogin().disable()
 
     return http.build()
+  }
+
+  @Bean
+  fun corsConfigurationSource(): CorsConfigurationSource {
+    val configuration = CorsConfiguration()
+    configuration.allowedOrigins = listOf("http://localhost:8081")
+    configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    configuration.allowedHeaders = listOf("*")
+    configuration.allowCredentials = true
+    val source = UrlBasedCorsConfigurationSource()
+    source.registerCorsConfiguration("/**", configuration)
+    return source
   }
 }
